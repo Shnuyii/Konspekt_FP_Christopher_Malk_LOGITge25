0f687e91c30a9022f806ef6fa7e9adbee5047689
Opilane TTHK <opilane@tthk.ee>
2026-03-19 14:46:10 +0200

    idk

---
+++ Esimene projekt/Vehicles/Laev.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esimene_projekt.Vehicles
+{
+    internal class Laev
+    {
+    }
+}
---
+++ Esimene projekt/Vehicles/Lennuk.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esimene_projekt.Vehicles
+{
+        public enum TravelClass
+        {
+            FirstClass, SecondClass, TouristClass, BaggageRoom
+        }
+    internal class Lennuk : Vehicle
+    {
+        public int TiivaLaius { get; set; }
+        public int LennuKiirus { get; set; }
+        public Lennuk(string model, CarMark mark, string plate, string colour, decimal unloadedWeight, List<string> equipment) : base(model, mark, plate, colour, unloadedWeight, equipment)
+        {
+        }
+    }
+}
